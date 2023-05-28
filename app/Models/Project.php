215d4ef304d66e5cf845536881295890fcbb9810
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-28 17:40:32 +0200

    img in json

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,5 +18,12 @@ class Project extends Model
 
     public function technologies(){
         return $this->belongsToMany(Technology::class)->withTimestamps();
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string|null $value) => $value !== null ?  asset('storage/' .$value) : null,
+        );
     }
 }
