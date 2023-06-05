7578c408be745e21e9393cfeeefb5a05716b4ce5
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-06-06 01:06:16 +0200

    api controller error

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,16 +11,21 @@ class ProjectController extends Controller
     public function index(){
 
        $projects = Project::with('type', 'technologies')->get();
-        //$projects = Project::->paginate(5);   //elementi per pagina (5) per la paginazione di molti elementi
+        //$projects = Project::->paginate(6);   //elementi per pagina (6) per la paginazione di molti elementi
 
         return response()->json( [
             'success'=> true,
             'results' => $projects
         ]);
     }
-    public function show(string $slug){
+    //public function show(int $id){ //chiamata all'API tramite ID e non più tramite slug
+        //$project = Project::where('id', $id)->with('type', 'technologies')->first();
+        
 
+public function show(string $slug){
+    try{
         $project = Project::where('slug', $slug)->with('type', 'technologies')->first();
+        
         if($project){
             return response()->json( [
                 'success'=> true,
@@ -32,6 +37,13 @@ class ProjectController extends Controller
                 'results' => null
             ], 404);
         }
+    } catch(\Throwable $th){
+        return response()->json( [
+            'success'=> false,
+            'results' => null
+        ], 500);
+    }
+    
 
     }
 }
